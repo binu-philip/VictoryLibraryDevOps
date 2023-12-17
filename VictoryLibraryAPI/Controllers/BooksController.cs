1bdbd767c841f506ed97822cae63f1411ee99f29
binu-philip <72704021+binu-philip@users.noreply.github.com>
2023-12-17 15:12:38 -0500

    Added Files

---
+++ VictoryLibraryAPI/Controllers/BooksController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VictoryLibraryAPI.Model;
+
+namespace VictoryLibraryAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        List<Book> books = new List<Book>();
+        public BooksController()
+        {          
+            books.Add(new Book { Id=1 ,Title = "If Tomorrow comes", Author = "Sydney Sheldon", Genere = "Novel" });
+            books.Add(new Book { Id=2,Title = "The Story of My Experiments with Truth", Author = "MK Gandhi", Genere = "AutoBiography" });
+        }
+        [HttpGet]
+        public List<Book> Get()
+        { 
+        
+          return books;
+        }
+        [HttpPost]
+        public List<Book> Post(Book book)
+        {
+            books.Add(book);
+            return books;
+        }
+        [HttpPut]
+        public List<Book> Put(Book book)
+        {
+            var selectedBook = books.FirstOrDefault(x => x.Id == book.Id);
+            books.Remove(selectedBook);           
+            books.Add(book);
+            return books;
+        }
+        [HttpDelete]
+        public List<Book> Delete(int id)
+        {
+            var selectedBook = books.FirstOrDefault(x=>x.Id==id);
+            books.Remove(selectedBook);         
+            return books;
+        }
+    }
+}
